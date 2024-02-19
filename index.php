9e831673889d8cb8a5afdfaccda5375e4637873e
Sir Varde <32492997+Simaneka@users.noreply.github.com>
2024-02-19 18:25:50 +0200

    US English to UK

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *"); // Allow requests from any origin (CORS)
 // header("Content-Type: application/json");
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorisation');
 
 
 
