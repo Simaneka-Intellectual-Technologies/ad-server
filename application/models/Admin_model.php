414d04562a4078adb09086b84315d36dbd66ded3
fkamukuju <fkamukuju@gmail.com>
2024-04-20 23:23:13 +0200

    Additionals

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -111,6 +111,11 @@ class Admin_model extends CI_Model
 	        /* Add cc or bcc */
 	        // $mail->addCC('info@simaneka.com');
 	        // $mail->addBCC('info@simaneka.com');
+
+
+
+
+
 	       
 	        /* Email subject */
 	        $mail->Subject = $client['client_name'] . ' Account Balance Notification';;
@@ -283,12 +288,15 @@ class Admin_model extends CI_Model
 		    <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td align="center" style="background-color: #ffffff;"><![endif]-->
 		
 		
+
+
 		
 		<div class="u-row-container" style="padding: 0px;background-color: transparent">
 		  <div class="u-row" style="margin: 0 auto;min-width: 320px;max-width: 550px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;">
 		    <div style="border-collapse: collapse;display: table;width: 100%;height: 100%;background-color: transparent;">
 		      <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:550px;"><tr style="background-color: transparent;"><![endif]-->
 		
+			  
 		<!--[if (mso)|(IE)]><td align="center" width="550" style="width: 550px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;" valign="top"><![endif]-->
 		<div class="u-col u-col-100" style="max-width: 320px;min-width: 550px;display: table-cell;vertical-align: top;">
 		  <div style="height: 100%;width: 100% !important;">
