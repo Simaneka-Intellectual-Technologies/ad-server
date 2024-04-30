63d297b174e7699e092ed8200e30e54315e81ec2
Simaneka <vardesimaneka@gmail.com>
2024-04-30 18:24:59 +0200

    Added verification

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -13,4 +13,51 @@ class Main extends CI_Controller
 			$reply
 		);
 	}
+	public function verify()
+	{
+		$id = $this->input->get('id');
+		$code = $this->input->get('code');
+
+		$where = array(
+			'user_id' => $this->input->get('id'),
+			'verfication_code' => $this->input->get('code'),
+		);
+
+		$data = array(
+			'verfication_code' => '',
+			'verified_on' => date('Y-m-d H:i:s'),
+			'status' => 1
+		);
+
+		$this->admin_model->update('admin', $where, $data);
+
+		$where2 = array(
+			'user_id' => $this->input->get('id'),
+		);
+
+		$user = $this->admin_model->get_where('admin', $where2, false, null);
+
+		if ($user) {
+			$user = $user[0];
+			$data = array(
+				'last_login' => date('Y-m-d H:i:s')
+			);
+			$where = array(
+				'user_id' => $user['user_id']
+			);
+			$this->admin_model->update('admin', $where, $data);
+			$user_data = array(
+				'user_id' => $user['user_id'],
+				'comp_id' => $user['comp_id'],
+				'email' => $user['email'],
+				'name' => $user['name'] . ' ' . $user['surname'],
+				'last_login' => $user['last_login'],
+				'type' => $user['type'],
+				'image' => $user['image'],
+				'logged_in' => true
+			);
+			$this->session->set_userdata($user_data);
+			header('Location: ' . base_url('admin/page/ads'));
+		}
+	}
 }
